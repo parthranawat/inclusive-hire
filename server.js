631e1ef732badcebb4cb71e5900f97ecebe9895d
Parth Ranawat <parth.ranawat@gmail.com>
2023-10-22 14:28:28 -0400

    some file fixes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT;
 const app = express();
 const server = http.createServer(app);
 const Routes = require("./app/routes");
-const path = require("path");
+const path = require('path');
 
 app.use([
   cors(),
@@ -17,20 +17,21 @@ app.use([
 ]);
 
 // const io = (module.exports.io = require("socket.io")(server));
-const io = (module.exports.io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
+// This is missing in the video.
+const io = (module.exports.io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
 }));
 const socketManager = require("./app/socketManager");
 io.on("connection", socketManager);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
