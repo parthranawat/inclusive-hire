7853a2454c8e59ba3b6640f98b322d486e87c7de
Parth Ranawat <parth.ranawat@gmail.com>
2023-10-23 19:45:14 -0400

    design changes

--- client/src/components/UI/Header/Header.js
+++ client/src/components/UI/Header/Header.js
@@ -4,14 +4,14 @@ import {
   faExclamationCircle,
   faCog,
 } from "@fortawesome/free-solid-svg-icons";
-import './Header.scss';
+import "./Header.scss";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="logo">
-        <img src="https://www.gstatic.com/meet/google_meet_horizontal_wordmark_2020q4_2x_icon_124_40_292e71bcb52a56e2a9005164118f183b.png" />
-        <span className="help-text">Meet</span>
+        <img src="https://cdn.vectorstock.com/i/preview-1x/24/55/monochrome-simple-interview-icon-vector-37872455.jpg" />
+        <span className="help-text">Inclusive Hire</span>
       </div>
       <div className="action-btn">
         <FontAwesomeIcon className="icon-block" icon={faQuestionCircle} />
